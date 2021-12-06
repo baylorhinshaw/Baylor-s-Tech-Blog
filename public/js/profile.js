f73e427a6e59750fc5ad9e497664d8c82011db93
Baylor <baylorhinshaw21@gmail.com>
2021-12-06 17:13:37 -0500

    added script to see if it will let me deploy to heroku

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,7 +5,7 @@ const newFormHandler = async (event) => {
   const description = document.querySelector('#post-description').value.trim();
 
   if (title && description) {
-    const response = await fetch(`/api/post`, {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({ title, description }),
       headers: {
